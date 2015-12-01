9166886baf773e297515fdb1d149a09ac3a25990
underscorediscovery <sven@underscorediscovery.com>
2015-12-01 07:38:10 -0330

    linc; don't apply touch/xml macros during completion

--- timestamp/Timestamp.hx
+++ timestamp/Timestamp.hx
@@ -2,8 +2,10 @@ package timestamp;
 
 @:keep
 @:include('linc_timestamp.h')
+#if !display
 @:build(linc.Linc.touch())
 @:build(linc.Linc.xml('timestamp'))
+#end
 extern class Timestamp {
 
     @:native('linc::timestamp::now')
